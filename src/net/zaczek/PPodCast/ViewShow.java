02118ee644c6dc6d2ab6564194f2ab1ca7d34194
Arthur Zaczek <arthur.zaczek@gmail.com>
2011-08-18 20:25:57 +0200

    Bugfixes

--- src/net/zaczek/PPodCast/ViewShow.java
+++ src/net/zaczek/PPodCast/ViewShow.java
@@ -21,15 +21,16 @@ import android.view.KeyEvent;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class ViewShow extends Activity implements OnErrorListener, OnBufferingUpdateListener, OnCompletionListener, OnInfoListener {
+public class ViewShow extends Activity implements OnErrorListener,
+		OnBufferingUpdateListener, OnCompletionListener, OnInfoListener {
 
 	private static final int STATUS_STOPPED = 1;
 	private static final int STATUS_PLAY = 2;
 	private static final int STATUS_BUFFERING = 3;
 	private static final int STATUS_ERROR = 4;
-	
+
 	private int status;
-	
+
 	private WakeLock wl;
 
 	private static final int DLG_WAIT = 1;
@@ -46,8 +47,6 @@ public class ViewShow extends Activity implements OnErrorListener, OnBufferingUp
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		updateStatus(STATUS_STOPPED);
 
 		PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
 		wl = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK, "ViewShowAndStayAwake");
@@ -68,23 +67,26 @@ public class ViewShow extends Activity implements OnErrorListener, OnBufferingUp
 		mp = new MediaPlayer();
 		mp.setScreenOnWhilePlaying(true); // TODO: Remove wake lock
 		am = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
-		
+
 		mp.setOnErrorListener(this);
 		mp.setOnBufferingUpdateListener(this);
 		mp.setOnCompletionListener(this);
 		mp.setOnInfoListener(this);
-		
+
+		updateStatus(STATUS_STOPPED);
 		fillData();
 	}
-	
+
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		switch (keyCode) {
 		case KeyEvent.KEYCODE_DPAD_DOWN:
-			am.adjustVolume(AudioManager.ADJUST_RAISE, AudioManager.FLAG_SHOW_UI);
+			am.adjustVolume(AudioManager.ADJUST_RAISE,
+					AudioManager.FLAG_SHOW_UI);
 			return true;
 		case KeyEvent.KEYCODE_DPAD_UP:
-			am.adjustVolume(AudioManager.ADJUST_LOWER, AudioManager.FLAG_SHOW_UI);
+			am.adjustVolume(AudioManager.ADJUST_LOWER,
+					AudioManager.FLAG_SHOW_UI);
 			return true;
 		case KeyEvent.KEYCODE_DPAD_RIGHT:
 		case KeyEvent.KEYCODE_MEDIA_NEXT:
@@ -94,11 +96,14 @@ public class ViewShow extends Activity implements OnErrorListener, OnBufferingUp
 		case KeyEvent.KEYCODE_MEDIA_PREVIOUS:
 			mp.seekTo(mp.getCurrentPosition() - 5000);
 			return true;
+		case KeyEvent.KEYCODE_DPAD_CENTER:
 		case KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE:
-			if(mp.isPlaying()) {
-				pause();
-			} else {
-				start();
+			if (status != STATUS_BUFFERING) {
+				if (mp.isPlaying()) {
+					pause();
+				} else {
+					start();
+				}
 			}
 			return true;
 		default:
@@ -143,31 +148,38 @@ public class ViewShow extends Activity implements OnErrorListener, OnBufferingUp
 
 		startManagingCursor(showCursor);
 
-		String title = showCursor.getString(showCursor.getColumnIndex(PuddleDbAdapter.SHOW_TITLE));
+		String title = showCursor.getString(showCursor
+				.getColumnIndex(PuddleDbAdapter.SHOW_TITLE));
 		showTitle.setText(title);
 
-		String description = showCursor.getString(showCursor.getColumnIndex(PuddleDbAdapter.SHOW_DESCRIPTION));
+		String description = showCursor.getString(showCursor
+				.getColumnIndex(PuddleDbAdapter.SHOW_DESCRIPTION));
 		showDescription.setText(description);
 
-	    String url = showCursor.getString(showCursor.getColumnIndex(PuddleDbAdapter.SHOW_URL));
+		String url = showCursor.getString(showCursor
+				.getColumnIndex(PuddleDbAdapter.SHOW_URL));
 		play(url);
 	}
+
 	private void updateStatus(String error) {
 		updateStatus(STATUS_ERROR, error);
-	}	
+	}
+
 	private void updateStatus(int newStatus) {
 		updateStatus(newStatus, "Unknown Error");
-	}	
+	}
+
 	private void updateStatus(int newStatus, String error) {
-		if(status == STATUS_ERROR && newStatus == STATUS_STOPPED) return;
+		if (status == STATUS_ERROR && newStatus == STATUS_STOPPED)
+			return;
 		status = newStatus;
-		
-		switch(status) {
+
+		switch (status) {
 		case STATUS_BUFFERING:
-			showStatus.setText("Playing");
+			showStatus.setText("Buffering");
 			break;
 		case STATUS_PLAY:
-			showStatus.setText("Buffering");
+			showStatus.setText("Playing");
 			break;
 		case STATUS_STOPPED:
 			showStatus.setText("Stopped");
@@ -187,19 +199,19 @@ public class ViewShow extends Activity implements OnErrorListener, OnBufferingUp
 					dismissDialog(DLG_WAIT);
 				}
 			});
-			updateStatus(STATUS_BUFFERING);	
+			updateStatus(STATUS_BUFFERING);
 			showDialog(DLG_WAIT);
 			mp.prepareAsync();
 		} catch (Exception e) {
 			Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
 		}
 	}
-	
+
 	private void start() {
 		mp.start();
 		updateStatus(STATUS_PLAY);
 	}
-	
+
 	private void pause() {
 		mp.pause();
 		updateStatus(STATUS_STOPPED);
@@ -208,7 +220,7 @@ public class ViewShow extends Activity implements OnErrorListener, OnBufferingUp
 	public void onBufferingUpdate(MediaPlayer mp, int percent) {
 		showStatusBuffer.setText(percent + "% Buf.");
 	}
-	
+
 	public boolean onError(MediaPlayer mp, int what, int extra) {
 		String error;
 		switch (what) {
@@ -226,17 +238,17 @@ public class ViewShow extends Activity implements OnErrorListener, OnBufferingUp
 
 	@Override
 	public void onCompletion(MediaPlayer mp) {
-		updateStatus(STATUS_STOPPED);		
+		updateStatus(STATUS_STOPPED);
 	}
 
 	@Override
 	public boolean onInfo(MediaPlayer mp, int what, int extra) {
 		// nothing interesting
-//		MEDIA_INFO_UNKNOWN
-//		MEDIA_INFO_VIDEO_TRACK_LAGGING
-//		MEDIA_INFO_BAD_INTERLEAVING
-//		MEDIA_INFO_NOT_SEEKABLE
-//		MEDIA_INFO_METADATA_UPDATE
+		// MEDIA_INFO_UNKNOWN
+		// MEDIA_INFO_VIDEO_TRACK_LAGGING
+		// MEDIA_INFO_BAD_INTERLEAVING
+		// MEDIA_INFO_NOT_SEEKABLE
+		// MEDIA_INFO_METADATA_UPDATE
 		return false;
 	}
 }
